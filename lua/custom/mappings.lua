5a1c72c6635d7cbda0a931458385c53cf55d7817
kubakakauko <47721520+kubakakauko@users.noreply.github.com>
2024-06-05 20:30:54 +0100

    none-ls lazy false, now it runs all the time which may not be too good.

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,8 +1,20 @@
 -- init.lua or lua/custom/mappings.lua
 
 -- Set up Colemak mappings for window navigation
+--
 -- Colemak mappings for window navigation
 vim.api.nvim_set_keymap("n", "<C-n>", "<C-w>h", { noremap = true, silent = true, desc = "Move to left window" })
 vim.api.nvim_set_keymap("n", "<C-e>", "<C-w>j", { noremap = true, silent = true, desc = "Move to bottom window" })
 vim.api.nvim_set_keymap("n", "<C-i>", "<C-w>k", { noremap = true, silent = true, desc = "Move to top window" })
 vim.api.nvim_set_keymap("n", "<C-o>", "<C-w>l", { noremap = true, silent = true, desc = "Move to right window" })
+
+-- Unmap conflicting keys in normal mode
+vim.api.nvim_del_keymap("n", "gc")
+vim.api.nvim_del_keymap("n", "gb")
+
+-- Reassign gc and gb for comment toggling if using a comment plugin like comment.nvim
+vim.api.nvim_set_keymap("n", "gc", "<Plug>(comment_toggle_linewise)", { noremap = false, silent = true })
+vim.api.nvim_set_keymap("n", "gb", "<Plug>(comment_toggle_blockwise)", { noremap = false, silent = true })
+
+-- Custom command to open WhichKey with a specific leader key
+vim.api.nvim_set_keymap("n", "<Leader>?", ":WhichKey<CR>", { noremap = true, silent = true })
--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -63,7 +63,7 @@ return {
 
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
+        ["<Leader>b"] = { desc = "Buffers" },
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
